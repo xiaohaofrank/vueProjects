892dbdcb2cf79b140ae8402ec718008da389bb98
mierhongyan <mierhongyan@163.com>
2019-04-17 17:23:54 +0800

    从vs code 到github,而不是本地git.

--- vue-quote/quote-vue-tpyzq/src/App.vue
+++ vue-quote/quote-vue-tpyzq/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <div>34234234</div>
   </div>
+ 
 </template>
 
 <script>
